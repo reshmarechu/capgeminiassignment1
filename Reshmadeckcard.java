69d0f0f8b5268791893335e56a872bb21992491e
reshmarechu <85102730+reshmarechu@users.noreply.github.com>
2021-10-12 11:48:01 +0530

    Add files via upload

---
+++ Reshmadeckcard.java
@@ -0,0 +1,32 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+
+// DeckOfCards class represents a deck of playing cards.
+public class DeckOfCards2 {
+  public static void main(String[] args) {
+    int[] deck = new int[52];
+    String[] suits = {"Spades", "Hearts", "Diamonds", "Clubs"};
+    String[] ranks = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+
+    // Initialize cards
+    for (int i = 0; i < deck.length; i++) {
+      deck[i] = i;
+    }
+
+    // Shuffle the cards
+    for (int i = 0; i < deck.length; i++) {
+      int index = (int)(Math.random() * deck.length);
+      int temp = deck[i];
+      deck[i] = deck[index];
+      deck[index] = temp;
+    }
+
+    // Display the all the cards
+    for (int i = 0; i < 52; i++) {
+      String suit = suits[deck[i] / 13];
+      String rank = ranks[deck[i] % 13];
+      System.out.println( rank + " of " + suit);
+    }
+  }
+}
